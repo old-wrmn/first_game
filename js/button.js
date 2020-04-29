e757b555a06139fbc3981f454b2db2e3324a1fcb
aditya waarman <moawarman@gmail.com>
2020-04-29 20:11:38 +0700

    license

--- js/button.js
+++ js/button.js
@@ -1,9 +1,9 @@
-    const buttonsContainer = document.getElementById("buttonsContainer");
-    for (let i = 0; i < 5; i++) {
-        const button = document.createElement("button");
-        button.innerText = i+1;
-        button.addEventListener("click", function() {
-            startGame(i)
-        })
-        buttonsContainer.appendChild(button);
-    }
+const buttonsContainer = document.getElementById("buttonsContainer");
+for (let i = 0; i < 5; i++) {
+    const button = document.createElement("button");
+    button.innerText = i+1;
+    button.addEventListener("click", function() {
+        startGame(i)
+    })
+    buttonsContainer.appendChild(button);
+}
